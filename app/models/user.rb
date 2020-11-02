4f8f90595c2141767ef190da51d0a36a6bf12f92
EC2 Default User <ec2-user@ip-172-31-46-134.ec2.internal>
2020-11-02 07:32:07 +0000

    make simple user page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  attachment :image
+  
   validates :name, presence: true, length: { in: 2..20 }
 end
