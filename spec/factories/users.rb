44ff5d37eed3c9ae7a318d7dcc72d4c3802688a5
EC2 Default User <ec2-user@ip-172-31-45-139.ec2.internal>
2020-11-02 09:13:21 +0000

    write rspec for public/users

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,4 +6,13 @@ FactoryBot.define do
     password { "testtest" }
     password_confirmation { "testtest" }
   end
+  
+  factory :user2, class: "User" do
+    name { "nana" }
+    introduction { "Beginner of English" }
+    email { "nana@com" }
+    password { "testtest" }
+    password_confirmation { "testtest" }
+  end
+
 end
