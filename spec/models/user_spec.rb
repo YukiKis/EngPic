ce372e665c243c75a063e14433bf388967d622bb
EC2 Default User <ec2-user@ip-172-31-46-134.ec2.internal>
2020-11-02 06:45:59 +0000

    Write rspec-code for user-login/registration, homes page

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user1) { build(:user1) }
+  context "on user validation" do
+    it "is valid" do
+      expect(user1).to be_valid
+    end
+    it "is invalid without name" do
+      user1.name = ""
+      expect(user1).to be_invalid
+    end
+    it "is invalid if name is too short" do
+      user1.name = "a"
+      expect(user1).to be_invalid
+    end
+    it "is invalid if name is too long" do
+      user1.name = "a"* 100
+      expect(user1).to be_invalid
+    end
+  end
 end
---
+++ spec/systems/headers_spec.rb
@@ -0,0 +1,52 @@
+require "rails_helper"
+
+RSpec.describe "header", type: :system do
+  let(:user1){ create(:user1) }
+  context "when not logged in" do
+    before do
+      visit root_path
+    end
+    it "has 'LOGO' for top page" do
+      expect(page).to have_link "LOGO", href: root_path
+    end
+    it "has 'HOME' for top page" do
+      expect(page).to have_link "Home", href: root_path
+    end
+    it "has 'About' for about page" do
+      expect(page).to have_link "About", href: about_path
+    end
+    it "has 'HowTo' for howto page" do
+      expect(page).to have_link "HowTo", href: howto_path
+    end
+    it "has 'SignUp' for new_user_registration page" do
+      expect(page).to have_link "SignUp", href: new_user_registration_path
+    end
+    it "has 'SignIn' for new_user_session_page" do
+      expect(page).to have_link "SignIn", href: new_user_session_path
+    end
+  end
+  context "when logged in as user" do
+    before do
+      visit new_user_session_path
+      fill_in "user[email]", with: user1.email
+      fill_in "user[password]", with: user1.password
+      click_button "SignIn"
+    end
+    it "has 'LOGO' for top page" do
+      expect(page).to have_link "LOGO", href: root_path
+    end
+    it "has 'Users' for users-index page" do
+      expect(page).to have_link "Users", href: users_path
+    end
+    it "has 'Words' for words-index page" do
+      expect(page).to have_link "Words", href: words_path
+    end
+    it "has 'MyDictionary' for dictionary-show page" do
+      expect(page).to have_link "MyDictionary", href: dictionary_path
+    end
+    it "has 'SignOut' for logout" do
+      expect(page).to have_link "SignOut", href: destroy_user_session_path
+    end
+  end
+
+end
