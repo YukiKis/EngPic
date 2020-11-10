eacdd67972a57ab105c63d29956c11ef5678ade8
EC2 Default User <ec2-user@ip-172-31-37-175.ec2.internal>
2020-11-10 06:16:50 +0000

    make mail feature everyday

---
+++ app/lib/batch/send_notify_email.rb
@@ -0,0 +1,8 @@
+class Batch::SendNotifyEmail
+  def self.send_notify_mail
+    User.all.each do |u|
+      NotificationMailer.send_notify_mail(u).deliver_now
+    end
+    p "全ユーザーにメールの送信が完了しました。"
+  end
+end
