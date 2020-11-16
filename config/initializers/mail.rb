4fc1bdfd0d34a23cefcdaafacd44ca259974727f
EC2 Default User <ec2-user@ip-172-31-37-175.ec2.internal>
2020-11-16 08:44:14 +0000

    change default-url-option

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,5 +1,5 @@
-ActionMailer::Base.default_url_options = { protocol: "https", host: "d897b06322f248eb81db05207789bfca.vfs.cloud9.us-east-1.amazonaws.com" }
 if Rails.env.production?
+  ActionMailer::Base.default_url_options = { protocol: "https", host: ENV["IP_ADDRESS"] }
   ActionMailer::Base.raise_delivery_errors = true
   ActionMailer::Base.delivery_method = :smtp;
   ActionMailer::Base.smtp_settings = {
@@ -12,8 +12,9 @@ if Rails.env.production?
     enable_starttls_auto: true
   }
 elsif Rails.env.development?
+  ActionMailer::Base.default_url_options = { protocol: "https", host: "d897b06322f248eb81db05207789bfca.vfs.cloud9.us-east-1.amazonaws.com" }
   ActionMailer::Base.delivery_method = :letter_opener
 else
-  ActionMailer::Base.default_url_options = { host: "localhost", port: 3000 }
+  ActionMailer::Base.default_url_options = { protocol: "https", host: "d897b06322f248eb81db05207789bfca.vfs.cloud9.us-east-1.amazonaws.com" }
   ActionMailer::Base.delivery_method = :test
 end
